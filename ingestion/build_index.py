90dbe88f65f84f76e0c3dfa26a0052ed1f2e39b5
rookie-creator <sahilvaidya34@gmail.com>
2026-07-07 19:34:34 +0530

    created the file structure and insatll all the required libraries

---
+++ ingestion/build_index.py
@@ -0,0 +1,1 @@
+"""Build vector index from documents."""
---
+++ ui.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="RAG System")
+st.title("RAG System")
+st.write("Streamlit UI placeholder")
